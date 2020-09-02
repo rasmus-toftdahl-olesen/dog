11824d37c04c80d411b94f2b5680d4ef6fff54b0
Rasmus Toftdahl Olesen <rtol@oticon.com>
2020-09-02 21:32:35 +0200

    arduino-cli is an alpine based docker image, adapt the entrypoint.sh to that

--- tests/dockers/arduino-cli-for-dog/entrypoint.sh
+++ tests/dockers/arduino-cli-for-dog/entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 if [[ ! -d $DOG_HOME ]]; then
     mkdir -p $DOG_HOME
 fi
 
 if ! grep -q "$DOG_GROUP:x:$DOG_GID" /etc/group; then
-    groupadd -g $DOG_GID $DOG_GROUP
+    addgroup -g $DOG_GID $DOG_GROUP
 fi
-useradd -u $DOG_UID -g $DOG_GID -c Self -d $DOG_HOME -s /bin/bash -M -N $DOG_USER
+adduser -u $DOG_UID -G $DOG_GROUP -g Self -h $DOG_HOME -s /bin/sh -H -D $DOG_USER
 chown $DOG_UID:$DOG_GID $DOG_HOME
 if [[ $DOG_AS_ROOT = "True" ]]; then
     exec "$@"
 else
     args="$@"
-    exec /usr/bin/sudo -u $DOG_USER --preserve-env=PATH,P4PORT,P4USER -- /bin/bash -c "LANG=en_US.UTF-8 $args"
+    exec /usr/bin/sudo -u $DOG_USER --preserve-env=PATH,P4PORT,P4USER -- /bin/sh -c "$args"
 fi

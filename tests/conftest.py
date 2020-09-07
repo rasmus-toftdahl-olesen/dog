655c1cdba04ae0a15720856a6193bbf2373de590
Rasmus Toftdahl Olesen <rtol@oticon.com>
2020-09-07 13:37:07 +0200

    Add a dog version of multiarch/crossbuild and use it for testing

---
+++ tests/conftest.py
@@ -0,0 +1,75 @@
+import sys
+from pathlib import Path
+import os
+import tempfile
+from typing import Tuple
+import subprocess
+import pytest
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from dog import VERSION as ACTUAL_DOG_VERSION
+
+DOG_PYTHON_UNDER_TEST = os.getenv('DOG_PYTHON_UNDER_TEST', sys.executable)
+
+
+@pytest.fixture
+def capstrip(capfd):
+    class CapStrip:
+        def get(self) -> Tuple[str, str]:
+            out, err = capfd.readouterr()
+            out, err = out.strip(), err.strip()
+            return out, err
+
+    return CapStrip()
+
+
+@pytest.fixture
+def uid() -> int:
+    if 'win32' in sys.platform:
+        return 1000
+    else:
+        return os.getuid()
+
+
+@pytest.fixture
+def my_dog() -> Path:
+    return (Path(__file__).parent.parent / 'dog.py').absolute()
+
+
+@pytest.fixture
+def call_dog(my_dog, tmp_path):
+    def call(*args: object):
+        cmd_line = [DOG_PYTHON_UNDER_TEST, str(my_dog)]
+        for arg in args:
+            cmd_line.append(str(arg))
+        return subprocess.run(cmd_line, cwd=tmp_path).returncode
+
+    yield call
+
+
+@pytest.fixture
+def dog_env():
+    if 'win32' in sys.platform:
+        return '%DOG%'
+    else:
+        return '$DOG'
+
+
+def append_to_dog_config(tmp_path: Path, extra_dog_config: str):
+    dog_config = tmp_path / 'dog.config'
+    old_config = dog_config.read_text()
+    new_config = old_config + extra_dog_config
+    dog_config.write_text(new_config)
+
+
+@pytest.fixture
+def system_temp_dir() -> str:
+    if sys.platform == 'win32':
+        basedir = os.environ['TEMP']
+    else:
+        basedir = os.getenv('RUNNER_TEMP', '/tmp')
+    ret: Path = Path(tempfile.mktemp('dog_tests', 'dog_tests', basedir))
+    ret.mkdir(parents=True, exist_ok=False)
+    yield str(ret)
+    os.removedirs(str(ret))
+
---
+++ tests/test_crossbuild_for_dog.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+import pytest
+from conftest import DOG_PYTHON_UNDER_TEST
+import subprocess
+
+RESOURCES = Path(__file__).parent / 'resources' / 'crossbuild-for-dog'
+
+@pytest.fixture
+def call_shell(my_dog, monkeypatch):
+    if 'win32' in sys.platform:
+        monkeypatch.setenv('DOG', f'"{DOG_PYTHON_UNDER_TEST}" "{my_dog}"')
+    else:
+        monkeypatch.setenv('DOG', f'{DOG_PYTHON_UNDER_TEST} {my_dog}')
+
+    def call(shell_string: str):
+        return subprocess.run(shell_string, shell=True, cwd=RESOURCES)
+
+    return call
+
+def test_make_creates_arm_targets(call_shell, capstrip, dog_env):
+    call_shell(f'{dog_env} make')
+    stdout, stderr = capstrip.get()
+    assert stderr == ''
+    assert 'CROSS_TRIPLE: aarch64-linux-gnu' in stdout
+    assert 'ARM aarch64' in stdout
+

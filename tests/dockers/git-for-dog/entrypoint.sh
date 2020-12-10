e48e2089c2bffbba4074fc41ccfcdf6ff9b028b6
Rasmus Franch Toftdahl Olesen <rtol@oticon.com>
2020-12-10 20:55:52 +0100

    Add dog enabled alpine/git docker

---
+++ tests/dockers/git-for-dog/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ ! -d $DOG_HOME ]]; then
+  mkdir -p $DOG_HOME
+fi
+
+if ! grep -q "$DOG_GROUP:x:$DOG_GID" /etc/group; then
+  addgroup -g $DOG_GID $DOG_GROUP
+fi
+adduser -u $DOG_UID -G $DOG_GROUP -g Self -h $DOG_HOME -s /bin/sh -H -D $DOG_USER
+chown $DOG_UID:$DOG_GID $DOG_HOME
+if [[ $DOG_AS_ROOT == "True" ]]; then
+  exec "$@"
+else
+  args="$@"
+  exec /usr/bin/sudo -u $DOG_USER --preserve-env=$DOG_PRESERVE_ENV -- /bin/sh -c "$args"
+fi

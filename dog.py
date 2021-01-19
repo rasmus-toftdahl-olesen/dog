5827c43e59771be0c2c146dbe08ad6d2bb3dfa3c
Rasmus Franch Toftdahl Olesen <rtol@oticon.com>
2021-01-19 13:42:37 +0100

    Bump version to 9

--- dog.py
+++ dog.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from typing import List, Dict, Union
 
 CONFIG_FILE = 'dog.config'
-VERSION = 8
+VERSION = 9
 
 DogConfig = Dict[str, Union[str, int, bool, Path, List[str], Dict[str, str]]]
 

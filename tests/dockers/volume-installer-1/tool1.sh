25aded8e85681bf26178ccdddabbb50b6a91e708
Lars Marqvart <lars@marqvart.com>
2022-02-25 13:59:56 +0100

    Add simple volumes-from test
    Use two volume installers to demostrate volumes-from setup using dog.
    
    Test is currently disabled as it will required dog v. 12 as well as the
    needed volume installer dockers pushed to e.g. dockerhub.

---
+++ tests/dockers/volume-installer-1/tool1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "tool1 ran"
---
+++ tests/test_volumes_from_dog.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+from pathlib import Path
+
+import pytest
+
+from conftest import DOG_PYTHON_UNDER_TEST
+
+RESOURCES = Path(__file__).parent / 'resources' / 'volumes-from-test'
+
+
+@pytest.fixture
+def call_shell(my_dog, monkeypatch):
+    if 'win32' in sys.platform:
+        monkeypatch.setenv('DOG', f'"{DOG_PYTHON_UNDER_TEST}" "{my_dog}"')
+    else:
+        monkeypatch.setenv('DOG', f'{DOG_PYTHON_UNDER_TEST} {my_dog}')
+
+    def call(shell_string: str):
+        return subprocess.run(shell_string, shell=True, cwd=RESOURCES)
+
+    return call
+
+
+@pytest.mark.skip(reason='volume-installer dockers needed')
+def test_run_tool1(call_shell, capstrip, dog_env):
+    call_shell(f'{dog_env} /opt/tool1/tool1.sh')
+    stdout, stderr = capstrip.get()
+    assert stderr == ''
+    assert 'tool1 ran' in stdout
+
+
+@pytest.mark.skip(reason='volume-installer dockers needed')
+def test_run_tool2(call_shell, capstrip, dog_env):
+    call_shell(f'{dog_env} /opt/tool2/tool2.sh')
+    stdout, stderr = capstrip.get()
+    assert stderr == ''
+    assert 'tool2 ran' in stdout

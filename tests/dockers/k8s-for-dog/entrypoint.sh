60b67d95c7d6fc70f73e6f6225088e5f3fa56f46
Rasmus <rtol@demant.com>
2022-10-07 23:06:49 +0200

    Add first k8s-for-dog version

---
+++ tests/dockers/k8s-for-dog/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ ! -d $DOG_HOME ]]; then
+  mkdir -p $DOG_HOME
+fi
+
+if ! grep -q "$DOG_GROUP:x:$DOG_GID" /etc/group; then
+  groupadd -g $DOG_GID $DOG_GROUP
+fi
+useradd -u $DOG_UID -g $DOG_GID -c Self -d $DOG_HOME -s /bin/bash -M -N $DOG_USER
+chown $DOG_UID:$DOG_GID $DOG_HOME
+if [[ $DOG_AS_ROOT == "True" ]]; then
+  exec "$@"
+else
+  args="$@"
+  exec /usr/bin/sudo -u $DOG_USER --preserve-env=$DOG_PRESERVE_ENV -- /bin/sh -c "$args"
+fi
